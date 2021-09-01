367fa7d00c091bb4f04e17458e895bf0eebbef4b
seno-b <bangshinho@gmail.com>
2021-09-02 00:11:29 +0900

    add: 과목 도메인 추가

---
+++ src/main/kotlin/com/senob/mathflat/domain/Subject.kt
@@ -0,0 +1,35 @@
+package com.senob.mathflat.domain
+
+import javax.persistence.*
+
+@Entity
+class Subject(subjectName: String){
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @Column(length = 100)
+    var subjectName: String = subjectName
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Subject
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return "Subject(id=$id, subjectName='$subjectName')"
+    }
+
+
+}
