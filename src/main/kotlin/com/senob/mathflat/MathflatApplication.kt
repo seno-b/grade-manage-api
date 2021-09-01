2080178cc2d0b9e58b4da33ea2a00b21b46702a1
seno-b <bangshinho@gmail.com>
2021-09-01 20:41:28 +0900

    Initial commit

---
+++ src/main/kotlin/com/senob/mathflat/MathflatApplication.kt
@@ -0,0 +1,11 @@
+package com.senob.mathflat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MathflatApplication
+
+fun main(args: Array<String>) {
+	runApplication<MathflatApplication>(*args)
+}
---
+++ src/test/kotlin/com/senob/mathflat/MathflatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.senob.mathflat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MathflatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
