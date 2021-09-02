b913d0209cd764487e20239b6525c75476b1b2a8
SHINHO BANG <wasco@grovesoft.net>
2021-09-02 17:36:55 +0900

    Test: 테스트 코드 작성

--- src/test/kotlin/com/senob/mathflat/domain/ClassRoomTest.kt
+++ src/test/kotlin/com/senob/mathflat/domain/ClassRoomTest.kt
@@ -1,23 +1,17 @@
 package com.senob.mathflat.domain
 
 import com.senob.mathflat.repository.ClassRoomRepository
-import com.senob.mathflat.repository.StudentRepository
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.Rollback
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 import javax.persistence.EntityManager
 
 @SpringBootTest
 @Transactional
-@Rollback(false)
-internal class StudentTest {
-
-    @Autowired
-    private lateinit var studentRepository: StudentRepository
-
+internal class ClassRoomTest {
     @Autowired
     private lateinit var classRoomRepository: ClassRoomRepository
 
@@ -25,21 +19,23 @@ internal class StudentTest {
     private lateinit var em: EntityManager
 
     @Test
-    fun `학생 클래스 매핑 테스트`() {
-
+    fun `강의 생성 테스트`() {
         var classRoom = ClassRoom("신규 강의 오픈!!", "허균", LocalDateTime.now(), LocalDateTime.now())
-        classRoomRepository.save(classRoom)
 
-        var student = Student(name = "홍길동", grade = 1, schoolName = "호형호제 학교")
-        student.classRooms = listOf(StudentClassRoom(student, classRoom))
-        studentRepository.save(student)
+        classRoomRepository.save(classRoom)
 
         em.flush()
         em.clear()
 
-        var findStudent = studentRepository.findById(student.id!!).get()
-        println(findStudent)
-        println(findStudent.classRooms)
+        val findClassRoom = classRoomRepository.findById(classRoom.id!!).get()
+
+        Assertions.assertThat(classRoom).isEqualTo(findClassRoom)
     }
 
+    @Test
+    fun `강의 수강생 목록 조회`() {
+
+        // data.sql 로 임의 데이터 생성하여 테스트 진행
+        classRoomRepository
+    }
 }
