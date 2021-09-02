549ba7aa6b0cbca7f495142874f8fe798f2c77ab
seno-b <bangshinho@gmail.com>
2021-09-02 21:31:38 +0900

    Test: 학생 강의 매핑 테스트 수정 강의 연관관계 매핑 테스트

--- src/test/kotlin/com/senob/mathflat/domain/StudentTest.kt
+++ src/test/kotlin/com/senob/mathflat/domain/StudentTest.kt
@@ -2,6 +2,7 @@ package com.senob.mathflat.domain
 
 import com.senob.mathflat.repository.ClassRoomRepository
 import com.senob.mathflat.repository.StudentRepository
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -25,21 +26,26 @@ internal class StudentTest {
     private lateinit var em: EntityManager
 
     @Test
-    fun `학생 클래스 매핑 테스트`() {
+    fun `학생 강의 매핑`() {
 
-        var classRoom = ClassRoom("신규 강의 오픈!!", "허균", LocalDateTime.now(), LocalDateTime.now())
+        var classRoom = ClassRoom("데이터베이스 초급 강의", "김영한", LocalDateTime.now(), LocalDateTime.now().plusMonths(1))
         classRoomRepository.save(classRoom)
 
-        var student = Student(name = "홍길동", grade = 1, schoolName = "호형호제 학교")
-        student.classRooms = listOf(StudentClassRoom(student, classRoom))
+        var student = Student(name = "방신호", grade = 1, schoolName = "백암고등학교")
         studentRepository.save(student)
 
+        val studentClassRoom = StudentClassRoom(student, classRoom)
+        em.persist(studentClassRoom)
         em.flush()
         em.clear()
 
         var findStudent = studentRepository.findById(student.id!!).get()
         println(findStudent)
-        println(findStudent.classRooms)
+        for (data in findStudent.classRooms) {
+            println("classRoom.id = ${data.classRoom.id}, classRoom.name = ${data.classRoom.name}")
+        }
+
+        Assertions.assertThat(classRoom.id).isEqualTo(findStudent.classRooms.get(0).classRoom.id)
     }
 
 }
