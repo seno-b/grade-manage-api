51e71aaf776d8fd4a0acad63f0b33b8c2091e328
seno-b <bangshinho@gmail.com>
2021-09-03 00:08:04 +0900

    Feat: 점수 repository 평균 점수 조회 기능 추가 과목 기반 평균 점수 조회, 학생이 참여한 강의 전체 과목 평균 점수 조회 기능 개발

--- src/main/kotlin/com/senob/mathflat/repository/GradeRepository.kt
+++ src/main/kotlin/com/senob/mathflat/repository/GradeRepository.kt
@@ -2,6 +2,13 @@ package com.senob.mathflat.repository
 
 import com.senob.mathflat.domain.Grade
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface GradeRepository : JpaRepository<Grade, Long> {
+
+    @Query("select AVG(grade.score) from Grade grade join grade.student student join grade.subject subject join subject.classRoom classroom where classroom.id = :classRoomId and student.id = :studentId")
+    fun findGradeAvgByStudentIdAndClassroomId(studentId: Long, classRoomId: Long): Long
+
+    @Query("select AVG(grade.score) from Grade grade join grade.subject subject where subject.id = :subjectId")
+    fun findGradeAvgBySubjectId(subjectId: Long): Long
 }
